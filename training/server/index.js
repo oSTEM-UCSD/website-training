67fd6a572d63b167b0d5efbdc252e7da56d2b3fe
Devon Rojas <devonmrojas@gmail.com>
2020-04-09 18:20:37 -0700

    Server alive

--- training/server/index.js
+++ training/server/index.js
@@ -4,12 +4,22 @@ const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
 
+const http = require("http");
+
 const cors = require('cors');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
+setTimeout(() => {
+  http.get("/");
+}, 30 * MINUTE);
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
